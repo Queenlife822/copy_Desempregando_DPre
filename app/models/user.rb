e11276caddc01a29362903a4df090a90f158425b
Kauane <kauane.sousa@to.estudante.senai.br>
2023-06-20 17:01:38 -0300

    Método profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :company
   has_many :applicants
+
+  def profile
+    Profile.find_by(:user => self.id)
+    
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create!(email: 'user@gmail.com', password: '123456', password_confirmation: '123456')
 
 
+if User.count == 0
+  User.create!(email: 'user@gmail.com', password: '123456', password_confirmation: '123456')
+end
 
-company = Company.create!(name: 'Company Tech', url: 'https://google.com', user_id: user.id)
-company.logo.attach(io: File.open("#{Rails.root}/spec/fixtures/images/logo-tech.jpeg"), filename: 'logo-tech.jpeg')
+if Company.count == 0
+  company = Company.create!(name: 'Company Tech', url: 'https://google.com', user_id: user.id)
+  company.logo.attach(io: File.open("#{Rails.root}/spec/fixtures/images/logo-tech.jpeg"), filename: 'logo-tech.jpeg')
 
-cities_and_states = [['CE', 'Fortaleza'], ['SP', 'Sao Paulo'], ['RJ', 'Rio de Janeiro']]
-5.times do |t|
-  city_and_state = cities_and_states.sample
-  Position.create!(
-    name: "Vaga #{t}", career: (0..6).to_a.sample, contract: (0..2).to_a.sample, remote: true, publish: true,
-    state: city_and_state.first, city: city_and_state.last, company_id: Company.last.id,
-    description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae fuga, nobis culpa quidem ipsum, quod exercitationem similique aliquam dolorem veniam praesentium! Adipisci ad deserunt illo, ab omnis distinctio quae at.',
-    summary: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae fuga, nobis culpa quidem ipsum, quod exercitationem similique aliquam dolorem veniam praesentium! Adipisci ad deserunt illo, ab omnis distinctio quae at.'
-  )
-  if Profile.count == 0
-    Profile.create!(name: 'Anunciante')
-    Profile.create!(name: 'Candidato')
+  cities_and_states = [['CE', 'Fortaleza'], ['SP', 'Sao Paulo'], ['RJ', 'Rio de Janeiro']]
+  5.times do |t|
+    city_and_state = cities_and_states.sample
+    Position.create!(
+      name: "Vaga #{t}", career: (0..6).to_a.sample, contract: (0..2).to_a.sample, remote: true, publish: true,
+      state: city_and_state.first, city: city_and_state.last, company_id: Company.last.id,
+      description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae fuga, nobis culpa quidem ipsum, quod exercitationem similique aliquam dolorem veniam praesentium! Adipisci ad deserunt illo, ab omnis distinctio quae at.',
+      summary: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae fuga, nobis culpa quidem ipsum, quod exercitationem similique aliquam dolorem veniam praesentium! Adipisci ad deserunt illo, ab omnis distinctio quae at.'
+    )
   end
+end
+
+if Profile.count == 0
+  puts "Semeando Profiles"
+  Profile.create!(name: 'Anunciante')
+  Profile.create!(name: 'Candidato')
+  puts "Semeados com sucesso"
+end
